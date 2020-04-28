e2dccb17865d5f57a569134757465d93c4609433
ftt moses lee <ftt.moses.lee@grabtaxi.com>
2020-04-29 01:42:58 +0800

    added logic to batch max 3 notifications together and changed grouped notifications to be an array to handle edge case of more than 3 notifications having the same date time together

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ class Main
     return if !file
     notifications_json = Utils.parse_json(file)
     return if !notifications_json
-  
+
     notifications_map = generate_notifications_map(notifications_json, user_id)
     grouped_notifications = group_notifications(notifications_map)
     sorted_notifications = sort_notifications_by_date(grouped_notifications)
@@ -15,6 +15,8 @@ class Main
     print_notifications(sorted_notifications)
   end
   
+  private
+
   def generate_notifications_map(notifications, user_id)
     notifications_map = Hash.new{ |hash, key| hash[key] = Hash.new{ |hash, key| hash[key] = {} } }
     
@@ -34,10 +36,9 @@ class Main
       notification_type_string = Notification.get_notification_type_string(notification_type_id)
       date_string = Utils.get_date_string(latest_date)
       to_print = "[#{date_string}] #{users_string} #{notification_type_string}"
-      grouped_notifications[latest_date] = to_print
-      grouped_notifications
+      grouped_notifications << { to_print: to_print, date: latest_date }
     end
-    grouped_notifications = Hash.new
+    grouped_notifications = []
     notifications_map.each do | target_id, notification_type_id |
       notifications_map[target_id].each do | notification_type_id, sender_id |
         notifications_per_type = notifications_map[target_id][notification_type_id]
@@ -48,10 +49,11 @@ class Main
         latest_date = 0
         users = ''
         _, earliest_date = sorted_by_date.first
-  
+        batch = 0
         sorted_by_date.each do | sender_id, date |
-          if date - earliest_date > 30 * 1000
+          if date - earliest_date > 30 * 1000 || batch == 3
             # this notification is more than 30 secs from earliest
+            # OR there are currently 3 notifications batched together
             # set new earliest date and batch the last few messages together
             grouped_notifications = set_notification_group(
               grouped_notifications: grouped_notifications, notification_type_id: notification_type_id,
@@ -60,6 +62,7 @@ class Main
             # start new batch by setting a new earliest date and reset users string
             earliest_date = date
             users = ''
+            batch = 0
           end
           if date > latest_date
             latest_date = date
@@ -69,6 +72,7 @@ class Main
           else
             users = users + ' and ' + sender_id
           end
+          batch += 1
         end
         grouped_notifications = set_notification_group(
           grouped_notifications: grouped_notifications, notification_type_id: notification_type_id,
@@ -77,14 +81,14 @@ class Main
     end
     grouped_notifications
   end
-  
+
   def sort_notifications_by_date(grouped_notifications)
-    grouped_notifications.sort_by { |date, notification| date }
+    grouped_notifications.sort_by { |notification| notification[:date] }
   end
-  
+
   def print_notifications(sorted_notifications)
-    sorted_notifications.each do | _, notification_string |
-      p notification_string
+    sorted_notifications.each do | notification |
+      p notification[:to_print]
     end
   end
 end
