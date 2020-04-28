312f6474c26bec3ad28a5242a2bc68f809452d9b
ftt moses lee <ftt.moses.lee@grabtaxi.com>
2020-04-28 22:40:40 +0800

    add date helper

--- utils.rb
+++ utils.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'date'
 
 module Utils
   def self.read_file(file_name)
@@ -8,6 +9,12 @@ module Utils
       p e.message
       return nil
     end
+  end
+
+  def self.get_date_string(date_in_ms)
+    date_in_seconds = date_in_ms/1000;
+    date_time = DateTime.strptime(date_in_seconds.to_s, '%s')
+    date_time.strftime('%F %T')
   end
 
   def self.parse_json(file)
