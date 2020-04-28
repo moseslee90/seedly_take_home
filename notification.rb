1c4bbf52426a0a22f6d4f53485f9f46f2a51d3c8
ftt moses lee <ftt.moses.lee@grabtaxi.com>
2020-04-28 22:42:55 +0800

    added method for converting notifications into a hashmap to group notifications for each question together and filter out notifications to self

--- notification.rb
+++ notification.rb
@@ -6,8 +6,22 @@ def main(file_name, user_id)
   return if !file
   notifications_json = Utils.parse_json(file)
   return if !notifications_json
+  notifications_map = generate_notifications_map(notifications_json, user_id)
+  p notifications_map
+end
+
+def generate_notifications_map(notifications, user_id)
+  notifications_map = Hash.new{ |hash, key| hash[key] = Hash.new{ |hash, key| hash[key] = {} } }
   
-  p notifications_json
+  notifications.each do |element|
+    notification = Notification.new(element)
+    if notification&.user_id == user_id && notification&.sender_id != user_id
+      date = Utils.get_date_string(notification.created_at)
+      notifications_map[notification&.target_id][notification&.notification_type_id][notification&.sender_id] = notification.created_at
+    end
+  end
+
+  notifications_map
 end
 
 main(ARGV[0], ARGV[1])
