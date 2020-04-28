985a08abdb02b7b6b01b16cc45e0077e4701cec3
ftt moses lee <ftt.moses.lee@grabtaxi.com>
2020-04-28 22:33:38 +0800

    added helper for parsing json

--- utils.rb
+++ utils.rb
@@ -1,7 +1,18 @@
+require 'json'
+
 module Utils
   def self.read_file(file_name)
     begin
       File.read(file_name)
+    rescue StandardError => e
+      p e.message
+      return nil
+    end
+  end
+
+  def self.parse_json(file)
+    begin
+      JSON.parse(file, :symbolize_names => true)
     rescue StandardError => e
       p e.message
       return nil
